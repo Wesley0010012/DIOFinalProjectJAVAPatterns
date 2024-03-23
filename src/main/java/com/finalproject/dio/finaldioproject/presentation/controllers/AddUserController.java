85a21a70547c656be93c8be5229c165b21f18c6a
Wesley0010012 <wglaurindo33@gmail.com>
2024-03-23 20:13:20 -0300

    refactor: add Exception catch prevention

--- src/main/java/com/finalproject/dio/finaldioproject/presentation/controllers/AddUserController.java
+++ src/main/java/com/finalproject/dio/finaldioproject/presentation/controllers/AddUserController.java
@@ -72,6 +72,8 @@ public class AddUserController implements Controller<UserDTO> {
             return ResponseEntity.ok().body("Passed all");
         } catch(Error e) {
             return HttpHelpers.internalServerError();
+        } catch(Exception e) {
+            return HttpHelpers.internalServerError();
         }
     }
     
