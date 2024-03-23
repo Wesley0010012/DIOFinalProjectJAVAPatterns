6cd736f4cf89e2f4a28582c253789880c14d55e5
Wesley0010012 <wglaurindo33@gmail.com>
2024-03-22 22:17:11 -0300

    chore: add SpringBoot structure

---
+++ src/main/java/com/finalproject/dio/finaldioproject/FinaldioprojectApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.dio.finaldioproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinaldioprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinaldioprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalproject/dio/finaldioproject/FinaldioprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.dio.finaldioproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinaldioprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
