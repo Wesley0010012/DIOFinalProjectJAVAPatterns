d759522eaca33ab085ec63f505abb64ecad29155
Wesley0010012 <wglaurindo33@gmail.com>
2024-03-22 22:55:30 -0300

    test: ensure AddUserController return 400 if no cep was provided

--- src/test/java/com/finalproject/dio/finaldioproject/AddUserControllerTests.java
+++ src/test/java/com/finalproject/dio/finaldioproject/AddUserControllerTests.java
@@ -62,4 +62,20 @@ class AddUserControllerTests {
 		assertEquals(sample.getStatusCode(), httpResponse.getStatusCode());
 		assertEquals(sample.getBody(), httpResponse.getBody());
 	}
+
+	@Test
+	@DisplayName("Should return 400 if no cep was provided")
+	void noCepWasProvided() {
+		UserDTO httpRequest = new UserDTO();
+		httpRequest.setName("any_name");
+		httpRequest.setEmail("any_email");
+		httpRequest.setCep("");
+		
+		ResponseEntity<String> httpResponse = sut.handle(httpRequest);
+
+		ResponseEntity<String> sample = HttpHelpers.badRequest(new MissingParamError("cep"));
+
+		assertEquals(sample.getStatusCode(), httpResponse.getStatusCode());
+		assertEquals(sample.getBody(), httpResponse.getBody());
+	}
 }
